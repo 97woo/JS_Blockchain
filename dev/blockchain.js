371c5bc42b6dbea0ef06955a040c9dfe69045040
97woo <kicaptain16@gmail.com>
2023-08-31 01:04:20 +0900

    create pendingTransaction, test transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,18 +1,18 @@
 function Blockchain (){
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 //블록생성
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce :nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
-    this.newTransaction = []; //이 코드 추가 이유는 새로운 블록을 만들때 새운 트랜잭션을 해당 블록에 추가 하기 때문에 
+    this.pendingTransactions = []; //이 코드 추가 이유는 새로운 블록을 만들때 새운 트랜잭션을 해당 블록에 추가 하기 때문에 
     this.chain.push(newBlock)
     return newBlock;
 }
@@ -29,7 +29,8 @@ Blockchain.prototype.createNewTransaction = function(amount,sender, recipient) {
         sender : sender,
         recipient : recipient
     };
-    this.newTransactions.push(newTransaction);
+    this.pendingTransactions.push(newTransaction);
+    return this.getLastBlock()['index'] + 1;
 }
 
 
--- dev/test.js
+++ dev/test.js
@@ -4,8 +4,8 @@ const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf')
 
-bitcoin.createNewBlock(12,'OIUOEREDHKHKD','78s97d4x6dsf')
+bitcoin.createNewTransaction(100,'WOO99FJFJDL9','HOO12GEDKG')
 
-bitcoin.createNewBlock(7889,'OIUOEREDHKHKD','78s97d4x6dsf')
+bitcoin.createNewBlock(568764,'ASSAFSA213SAD','S8A1DAS0D24V8S')
 
-console.log(bitcoin)
+console.log(bitcoin.chain[1])
