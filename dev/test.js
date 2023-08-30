d59bbe0facdb1dcc9dd6dca56d3c4a83acf92192
97woo <kicaptain16@gmail.com>
2023-08-30 23:50:12 +0900

    getlastBlock

--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,11 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-console.log(bitcoin)
+
+bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf')
+
+bitcoin.createNewBlock(12,'OIUOEREDHKHKD','78s97d4x6dsf')
+
+bitcoin.createNewBlock(7889,'OIUOEREDHKHKD','78s97d4x6dsf')
+
 console.log(bitcoin)
